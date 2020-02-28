dd17563fce909bb06a3189f3ec5d5c9982bbec3d
mutamamQ <59139862+mutamamQ@users.noreply.github.com>
2020-02-28 23:07:18 +0300

    Add files via upload

---
+++ phaseChanges.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+char materials[]={"water"};
+//materials name
+double fuse[]={333.55,58.99};
+double vapor[]={2257,480.6};
+/*latent heat of fusion, latent heat of vaporization */
+char inp[100];
+int matNo; //the array number of the material and property
+int weight;
+double result;
+ //function to identify the correct number for materials array
+void materialno(){
+    printf("what material are you converting\n");
+    scanf("%s", inp);
+    for (int i = 0; i < 30; i++){
+
+        if(strcmp(&materials[i],inp) == 0){
+            matNo = i;
+            printf("What is the mass in grams\n");
+            scanf("%d", &weight);
+            return;
+        }
+    }
+}
+void FOrV(){
+    printf("freezing/melting or vaporization/condensation\n");
+    scanf("%s", inp);    
+    if (strcmp(inp,"freezing") == 0 || strcmp(inp,"melting") == 0)
+    {   
+        result = fuse[matNo]*weight;
+        printf("The amount of energy needed to covert is %f joules\n", result);
+        return;
+    }
+    else if (strcmp(inp,"vaporization") == 0 || strcmp(inp,"condensation") == 0)
+    {   
+        result = vapor[matNo]*weight;
+        printf("The amount of energy needed to covert is %f joules\n", result);
+        return;
+    }
+}
+int main(){
+    materialno();
+    FOrV();
+}
